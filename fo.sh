30e17d76daf37331b031c07bd205d29fdfaa82ec
Vui-Chee <vuicheesiew@gmail.com>
2019-11-13 15:03:27 +0800

    Reorganized file checking logic.
    Among changes:
    1. Do not include filepath as arg (for now)
    2. .sh files are still treated as executables

--- fo.sh
+++ fo.sh
@@ -4,7 +4,7 @@ function fo() {
 
     # Install brew
     if [ ! -x "$(which brew)" ];then
-        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+      /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     fi
 
     # Install fzf 
@@ -15,28 +15,31 @@ function fo() {
     [ ! -x "$(which bat)" ] && brew install bat
 
     local out filepath key input
-    local lastArg="${@: -1}"
-    input='fd . '$lastArg' -H '
+    input='fd . -H '
     IFS=$'\n' 
     out=($(eval $input | fzf --preview-window down:10 --preview='[[ $(file --mime {}) =~ binary ]] && echo {} is a binary file || (bat --style=numbers --color=always {} || cat{}) 2> /dev/null | head -200' --exit-0))
-    filepath=$(head -2 <<< "$out" | tail -1)
+    filepath="$(pwd)/$(head -2 <<< "$out" | tail -1)"
 
-    local ext="${filepath##*.}" 
-    
+    # Enter directory
     if [ -d "$filepath" ];then
-        if [ "$ext" = "app" ];then
-            open "$filepath"
-        else
-            cd "$filepath"
-        fi
+      cd "$filepath"
+      return
     fi
 
-    if [ -f "$filepath" ] && [ ! -x "$filepath" ];then
-      if [[ $(file -b "$filepath") =~ (JPEG|PDF|PNG|JPEG|GIF) ]];then
-          open "$filepath"
-      else
-          # Use your favourite editor. (In my case, it's Neovim)
-          ${EDITOR:-nvim} "$filepath"
+    # All else must be a file to continue.
+    if [ ! -f "$filepath" ];then
+      echo "$filepath is not a file."
+      return
+    fi
+
+    if [[ $(file -b "$filepath") =~ (JPEG|PDF|PNG|JPEG|GIF) ]];then
+        open "$filepath"
+    else
+      if [ -x "$filepath" ];then
+        echo "$filepath is an executable."
+        return
       fi
+      # Use your favourite editor. (In my case, it's Neovim)
+      ${EDITOR:-nvim} "$filepath"
     fi
 }
